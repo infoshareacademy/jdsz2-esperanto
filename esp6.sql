2d0f45051898d8578f21c22acacfaa768359346d
Justyna Krygier <justynakrygier@yahoo.com>
2019-01-19 08:24:47 +0100

    Delete esp6.sql

--- esp6.sql
+++
@@ -1,7 +0,0 @@
---liczba wniosków złożona przez jednego klienta,
--- weryfikacja po mailu
--- wniosek bezużyeczne, zaszyfrowane dane
-SELECT email, count(1)
-FROM klienci
-GROUP BY email
-ORDER BY count(1) desc;
