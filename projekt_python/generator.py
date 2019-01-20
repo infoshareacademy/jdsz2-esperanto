dc8bba50810e2f616d74e99abfcd2b3b9d1d9b9e
Karolina Wojciechowska <karwojciechowska@gmail.com>
2019-01-20 13:55:15 +0100

    0.1

--- projekt_python/generator.py
+++ projekt_python/generator.py
@@ -1,16 +1,27 @@
 import numpy as np
+import scipy as sp
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-def generator_liczb_losowych(xmin, xmax, funkcja_rozkladu):
-    lista = funkcja_rozkladu(size=1000)
-    losowa = None
-    r = np.random.uniform()
-    for i in lista:
-        if i in [xmin, xmax] and r < i/lista.max:
-            losowa = i
-            break
-    return losowa
+
+def generator_liczb_losowych(xmin, xmax, n):
+    max_wartosc_funkcji = max([norm.pdf(c) for c in np.arange(xmin, xmax, (xmax-xmin)/n)])
+    print(max_wartosc_funkcji, [x for x in np.arange(xmin, xmax, (xmax-xmin)/n)])
+    losowe = []
+    count = 0
+    while len(losowe) < n:
+        r = np.random.uniform()
+        x = np.random.uniform(low=xmin, high=xmax)
+        if r < norm.pdf(x)/max_wartosc_funkcji:
+            losowe.append(x)
+        else:
+            count += 1
+    return losowe, count
+    # TODO: zmienia się norm.pdf i x = funkcja
 
 if __name__ == '__main__':
-    wynik = generator_liczb_losowych(1, 5, np.random.normal)
+    wynik, count = generator_liczb_losowych(-3, 3, 1000)
     print(wynik)
+    print(count)
+    plt.hist(wynik)
+    plt.show()
