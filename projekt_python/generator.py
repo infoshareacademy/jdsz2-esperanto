4fe5dde339938f5cde52c8eb9c2bfeb0dd8843c4
Karolina Wojciechowska <karwojciechowska@gmail.com>
2019-02-08 10:03:46 +0100

    update

--- projekt_python/generator.py
+++ projekt_python/generator.py
@@ -1,12 +1,14 @@
+import statistics
+import sys
+import time
+
 import numpy as np
-import scipy as sp
-from scipy.stats import norm
-import matplotlib.pyplot as plt
+from scipy.stats import norm, kstest
 
 
 def generator_liczb_losowych(xmin, xmax, n):
     max_wartosc_funkcji = max([norm.pdf(c) for c in np.arange(xmin, xmax, (xmax-xmin)/n)])
-    print(max_wartosc_funkcji, [x for x in np.arange(xmin, xmax, (xmax-xmin)/n)])
+    # print(max_wartosc_funkcji, [x for x in np.arange(xmin, xmax, (xmax-xmin)/n)])
     losowe = []
     count = 0
     while len(losowe) < n:
@@ -17,11 +19,19 @@ def generator_liczb_losowych(xmin, xmax, n):
         else:
             count += 1
     return losowe, count
-    # TODO: zmienia się norm.pdf i x = funkcja
+
 
 if __name__ == '__main__':
-    wynik, count = generator_liczb_losowych(-3, 3, 1000)
-    print(wynik)
-    print(count)
-    plt.hist(wynik)
-    plt.show()
+    count = 50
+    ds = []
+    ps = []
+    while count > 0:
+        wynik, miss_count = generator_liczb_losowych(-3, 3, 1000)
+        d, p = kstest(wynik, 'norm')
+        ds.append(d)
+        ps.append(p)
+        count = count - 1
+        time.sleep(1)
+        sys.stdout.write("\rZostało jeszcze kroków: %d" % count)
+        sys.stdout.flush()
+    print("\n", statistics.mean(ds), statistics.mean(ps))
