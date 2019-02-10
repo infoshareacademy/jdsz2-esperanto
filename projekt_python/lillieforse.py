37cd2ba1f6e070321a7fb3d8532fb0aed6da194b
KMdev94 <karolmularski94@gmail.com>
2019-02-10 19:13:39 +0100

    moje komentarze

--- projekt_python/lillieforse.py
+++ projekt_python/lillieforse.py
@@ -1,14 +1,10 @@
-import statistics
-import sys
-import time
-import pickle
 import numpy as np
-from scipy.stats import norm, kstest, shapiro
+from scipy.stats import norm
 from statsmodels.stats.diagnostic import lilliefors
-
-import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
-
+# TODO: Po skończeniu pisania programu warto wcisnąć CTRL ALT O, celem zoptymalizowania importów - usuwa nieużywane
+#  impoty i ładnie segreguje pozostałe. Ponadgo jak istalujesz jakąś zewnętrzną bibliotekę to od razu dodaj ją do
+#  requirements.txt, dzięki czemu jak będziesz chciała w przyszłości odpalić projekt to wystarczy wpisać pip install
+#  -r requirements.txt.
 
 
 def generator_liczb_losowych(xmin, xmax, n):
@@ -24,13 +20,14 @@ def generator_liczb_losowych(xmin, xmax, n):
     return losowe
 
 
-# Test Lillieforsa
-def L(x, alfa=0.05):
+def test_lillieforsa(x, alfa=0.05):
     D, p_l = lilliefors(x, 'norm')#, args=(0, 1))
     if p_l < alfa:
         return 0
-    else:
-        return 1
+    return 1
+    # TODO: Nie trzeba tu dawać else - jeżeli if będzie spełniony to funkcja zakończy swoje działanie w kroku 'return 0'
+    #  Natomiast jeżeli warunek nie zostanie spełniony to przejdzie do return 1. Ponadto warto nazywać funckje tak jak
+    #  tutaj. "L" mówi Ci coś tylko parę tygodni po napisaniu funkcji a innym deweloperom już w ogóle nic
 
 
 liczba_l = 0
@@ -39,18 +36,21 @@ n = 10
 u = 100
 
 for i in range(n):
-    i = generator_liczb_losowych(-3, 3, u)
-    x = i
-    L(x)
-    liczba_l += L(x)
+    x = generator_liczb_losowych(-3, 3, u)
+    test_lillieforsa(x)
+    liczba_l += test_lillieforsa(x)
 
 liczba_los = 0
 for i in range(n):
-    i = norm.rvs(size = u)
-    x = i
-    L(x)
-    liczba_los += L(x)
-sys.stdout = open('results_lillieforse.csv', 'a')
-print('Test L dla {} losowan z generatora {} liczb: {}'.format(n, u, liczba_l/n))
-print('Test L dla {} losowan z normalnego {} liczb: {}'.format(n, u, liczba_los/n))
-sys.stdout.close()
+    x = norm.rvs(size=u)
+    test_lillieforsa(x)
+    liczba_los += test_lillieforsa(x)
+    # TODO: tutaj nie było konieczności nadpisywać zmiennej i. Tak jest dużo czytelniej - wiadomo że i mówi tylko o
+    #  indeksie iteracji a x jest zmienną jakiejś funkcji matematycznej.
+
+with open('results_lillieforse.csv', 'a+') as file:
+    file.write('Test L dla {} losowan z generatora {} liczb: {}'.format(n, u, liczba_l/n))
+    file.write('Test L dla {} losowan z normalnego {} liczb: {}'.format(n, u, liczba_los/n))
+# TODO: 'with' gwarantuje, że jak coś się wykrzaczy w bloku pod nim to pliki, zmienne, pamięć itd. zostanie w stanie
+#  sprzed tego bloku. Dodatkowo nie musisz nużywać stodout, czyli nie musisz nadpisywać defaultowego struminia zapisu
+#  danych
