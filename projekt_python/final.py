b75f449a6061263fc486510ea6ed31f5cbbbb49d
Justyna Krygier <justynakrygier@yahoo.com>
2019-02-10 07:33:31 +0100

    UPdate KS test i wyniki

--- projekt_python/final.py
+++ projekt_python/final.py
@@ -30,7 +30,7 @@ def SW(x,alfa=0.05):
 
 # Test Kolmogorova-Smirnova
 def KS(x,alfa=0.05):
-    D, p_ks = kstest(x, 'norm', args=(np.mean(x), np.std(x, ddof=1)))
+    D, p_ks = kstest(x, 'norm') # args=(np.mean(x), np.std(x, ddof=1)))
     if p_ks < alfa:
         return 0
     else:
@@ -40,23 +40,23 @@ def KS(x,alfa=0.05):
 liczba_ks = 0
 liczba_sw = 0
 
-n = 10
-u = 1000
+n = 100
+u = 2000
 
 for i in range(n):
-    i = norm.rvs(size=u)
-#   i = generator_liczb_losowych(-3, 3, u)
+#    i = norm.rvs(size=u)
+    i = generator_liczb_losowych(-3, 3, u)
     x = i
     KS(x)
     SW(x)
     liczba_ks += KS(x)
     liczba_sw += SW(x)
 
-sys.stdout = open('results.csv', 'a')
-#print('generator Ko-Sm, {}, {}, {}'.format(n, u, liczba_ks/n))
-#print('generator Sh-Wi, {}, {}, {}'.format(n, u, liczba_sw/n))
-print('losowe Ko-Sm, {}, {}, {}'.format(n, u, liczba_ks/n))
-print('losowe Sh-Wi, {}, {}, {}'.format(n, u, liczba_sw/n))
+sys.stdout = open('results2.csv', 'a')
+print('generator Ko-Sm, {}, {}, {}'.format(n, u, liczba_ks/n))
+print('generator Sh-Wi, {}, {}, {}'.format(n, u, liczba_sw/n))
+#print('losowe Ko-Sm, {}, {}, {}'.format(n, u, liczba_ks/n))
+#print('losowe Sh-Wi, {}, {}, {}'.format(n, u, liczba_sw/n))
 sys.stdout.close()
 
 (mu, sigma) = norm.fit(x)
