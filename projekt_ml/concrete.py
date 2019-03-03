8ea4cf88134c89d98cc6d9d033aa4e50dffbef75
Justyna Krygier <justynakrygier@yahoo.com>
2019-03-03 19:59:01 +0100

    Decision Tree plus Cross Validations

--- projekt_ml/concrete.py
+++ projekt_ml/concrete.py
@@ -4,17 +4,16 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn import metrics
-
 from sklearn.preprocessing import Imputer
 from sklearn.metrics import mean_absolute_error, make_scorer, mean_squared_error
 from sklearn.model_selection import cross_val_score, KFold
+from sklearn.tree import DecisionTreeRegressor
 
 ## --------------------- import dataset ---------------------
 ## https://archive.ics.uci.edu/ml/datasets/Concrete+Compressive+Strength
 pd.set_option('display.max_columns',10)
 dataset = pd.read_csv('Concrete_Data.csv',quotechar='"', decimal=',', skipinitialspace=True)
 dataset.to_csv('Concrete_Corr', sep=',', encoding='utf-8', quotechar='"', decimal='.')
-
 
 ## Features -- quantitative -- kg in a m3 mixture -- Input Variables:
 # 1-Cement, 2-Blast Furnace Slag, 3-Fly Ash, 4-Water, 5-Superplasticizer, 6-Coarse Aggregate, 7-Fine Aggregate
@@ -30,6 +29,7 @@ columns_to_model = ['Cement', 'Blast Furnace Slag', 'Fly Ash', 'Water', 'Superpl
 X = dataset[columns_to_model]
 y = dataset['Concrete compressive strength']
 #print(y.head())
+#print(max(y),min(y)) =   82.6 <-> 2.33
 
 
 ## ------------- feature dependencies on target -------------
@@ -39,7 +39,7 @@ for i in columns_to_model:
 #    plt.show()
 
 
-## ------------ dataset training and regression -------------
+## -------- dataset training and linear regression ----------
 # podzial datasetu na dane treningowe i testowe
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25, random_state=101)
 
@@ -49,6 +49,9 @@ regr.fit(X_train, y_train)
 y_pred = regr.predict(X_test)
 
 # The coefficient:
+print('________________________________________________________')
+print('                    LinearRegression                    ')
+print('________________________________________________________')
 print('Wyraz wolny:', regr.intercept_)
 print('Wspolczynniki:', regr.coef_)
 print('R^2:', metrics.r2_score(y_test, y_pred))
@@ -62,12 +65,35 @@ my_imputer = Imputer()
 train_X = my_imputer.fit_transform(X_train)
 test_X = my_imputer.transform(X_test)
 
+print('               +++ Cross Validation  +++               ')
 kfold = KFold(n_splits=20, random_state=11)
 
 scorer_MAE = make_scorer(mean_absolute_error)
 results = cross_val_score(regr, X_train, y_train, cv=kfold, scoring=scorer_MAE)
-print('==== Min_MAE:{}, Max_MAE:{} ===='.format(min(results), max(results)))
-
+print('Min_MAE:  {}   Max_MAE:  {}'.format(min(results), max(results)))
 scorer_MSE = make_scorer(mean_squared_error)
 results = cross_val_score(regr, X_train, y_train, cv=kfold, scoring=scorer_MSE)
-print('==== Min_MSE:{}, Max_MSE:{} ===='.format(min(results), max(results)))
+print('Min_MSE:  {}   Max_MSE:  {}'.format(min(results), max(results)))
+
+
+## --------------- DecisionTreeRegressor ----------------
+print('________________________________________________________')
+print('                  DecisionTreeRegressor                 ')
+print('________________________________________________________')
+for max_features in [1,2,3,4,5,6,7,8]:
+    model = DecisionTreeRegressor(max_features=max_features, random_state=None, max_depth=None,
+                                  min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0)
+    model.fit(X_train, y_train)
+    preds_val = model.predict(X_test)
+    mae = mean_absolute_error(y_test, preds_val)
+    print('For max features: {}    Mean Absolute Error:  {}'.format(max_features, mae))
+
+print('                +++ Cross Validation  +++               ')
+kfold = KFold(n_splits=20, random_state=11)
+
+scorer_MAE = make_scorer(mean_absolute_error)
+results = cross_val_score(model, X_train, y_train, cv=kfold, scoring=scorer_MAE)
+print('Min_MAE:  {}   Max_MAE:  {}'.format(min(results), max(results)))
+scorer_MSE = make_scorer(mean_squared_error)
+results = cross_val_score(regr, X_train, y_train, cv=kfold, scoring=scorer_MSE)
+print('Min_MSE:  {}   Max_MSE:  {}'.format(min(results), max(results)))
