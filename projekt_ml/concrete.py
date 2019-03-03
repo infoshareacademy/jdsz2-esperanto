dcec99e5d993c2b8f4d8d0932d0b82ea4ac1b4de
Justyna Krygier <justynakrygier@yahoo.com>
2019-03-03 20:29:37 +0100

    Update cross-val

--- projekt_ml/concrete.py
+++ projekt_ml/concrete.py
@@ -43,15 +43,28 @@ for i in columns_to_model:
 # podzial datasetu na dane treningowe i testowe
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25, random_state=101)
 
+
+## ------------- cross validations MAE / MSE --------------
+my_imputer = Imputer()
+train_X = my_imputer.fit_transform(X_train)
+test_X = my_imputer.transform(X_test)
+
+kfold = KFold(n_splits=20, random_state=11)
+
+scorer_MAE = make_scorer(mean_absolute_error)
+scorer_MSE = make_scorer(mean_squared_error)
+
+
+print('________________________________________________________')
+print('                    LinearRegression                    ')
+print('________________________________________________________')
+
 # regresja liniowa:
 regr = linear_model.LinearRegression(normalize=True)
 regr.fit(X_train, y_train)
 y_pred = regr.predict(X_test)
 
 # The coefficient:
-print('________________________________________________________')
-print('                    LinearRegression                    ')
-print('________________________________________________________')
 print('Wyraz wolny:', regr.intercept_)
 print('Wspolczynniki:', regr.coef_)
 print('R^2:', metrics.r2_score(y_test, y_pred))
@@ -59,41 +72,29 @@ print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
 print('Median Absolute Error:', metrics.median_absolute_error(y_test, y_pred))
 
+print('                +++ Cross Validation  +++               ')
+results_MAE = cross_val_score(regr, X_train, y_train, cv=kfold, scoring=scorer_MAE)
+results_MSE = cross_val_score(regr, X_train, y_train, cv=kfold, scoring=scorer_MSE)
 
-## ------------- cross validations MAE / MSE --------------
-my_imputer = Imputer()
-train_X = my_imputer.fit_transform(X_train)
-test_X = my_imputer.transform(X_test)
-
-print('               +++ Cross Validation  +++               ')
-kfold = KFold(n_splits=20, random_state=11)
-
-scorer_MAE = make_scorer(mean_absolute_error)
-results = cross_val_score(regr, X_train, y_train, cv=kfold, scoring=scorer_MAE)
-print('Min_MAE:  {}   Max_MAE:  {}'.format(min(results), max(results)))
-scorer_MSE = make_scorer(mean_squared_error)
-results = cross_val_score(regr, X_train, y_train, cv=kfold, scoring=scorer_MSE)
-print('Min_MSE:  {}   Max_MSE:  {}'.format(min(results), max(results)))
+print('Min_MAE:  {}   Max_MAE:  {}'.format(min(results_MAE), max(results_MAE)))
+print('Min_MSE:  {}   Max_MSE:  {}'.format(min(results_MSE), max(results_MSE)))
 
 
-## --------------- DecisionTreeRegressor ----------------
 print('________________________________________________________')
 print('                  DecisionTreeRegressor                 ')
 print('________________________________________________________')
 for max_features in [1,2,3,4,5,6,7,8]:
-    model = DecisionTreeRegressor(max_features=max_features, random_state=None, max_depth=None,
+    dtree = DecisionTreeRegressor(max_features=max_features, random_state=None, max_depth=None,
                                   min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0)
-    model.fit(X_train, y_train)
-    preds_val = model.predict(X_test)
-    mae = mean_absolute_error(y_test, preds_val)
-    print('For max features: {}    Mean Absolute Error:  {}'.format(max_features, mae))
+    dtree.fit(X_train, y_train)
+    y_pred = dtree.predict(X_test)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = metrics.r2_score(y_test, y_pred)
+    print('For max features: {}    Mean Absolute Error:  {}    R^2:  {}'.format(max_features, mae, r2))
 
 print('                +++ Cross Validation  +++               ')
-kfold = KFold(n_splits=20, random_state=11)
+results_MAE = cross_val_score(dtree, X_train, y_train, cv=kfold, scoring=scorer_MAE)
+results_MSE = cross_val_score(dtree, X_train, y_train, cv=kfold, scoring=scorer_MSE)
 
-scorer_MAE = make_scorer(mean_absolute_error)
-results = cross_val_score(model, X_train, y_train, cv=kfold, scoring=scorer_MAE)
-print('Min_MAE:  {}   Max_MAE:  {}'.format(min(results), max(results)))
-scorer_MSE = make_scorer(mean_squared_error)
-results = cross_val_score(regr, X_train, y_train, cv=kfold, scoring=scorer_MSE)
-print('Min_MSE:  {}   Max_MSE:  {}'.format(min(results), max(results)))
+print('Min_MAE:  {}   Max_MAE:  {}'.format(min(results_MAE), max(results_MAE)))
+print('Min_MSE:  {}   Max_MSE:  {}'.format(min(results_MSE), max(results_MSE)))
