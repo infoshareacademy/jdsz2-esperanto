49fa1d079382726d6268f4b3bf617a2d8198d4ec
Justyna Krygier <justynakrygier@yahoo.com>
2019-04-13 14:26:41 +0200

    UPdate bottleneck

---
+++ projekt_dl/model_bottleneck.py
@@ -0,0 +1,93 @@
+'''This script goes along the blog post
+"Building powerful image classification models using very little data"
+from blog.keras.io.
+It uses data that can be downloaded at:
+https://www.kaggle.com/c/dogs-vs-cats/data
+In our setup, we:
+- created a data/ folder
+- created train/ and validation/ subfolders inside data/
+- created cats/ and dogs/ subfolders inside train/ and validation/
+- put the cat pictures index 0-999 in data/train/cats
+- put the cat pictures index 1000-1400 in data/validation/cats
+- put the dogs pictures index 12500-13499 in data/train/dogs
+- put the dog pictures index 13500-13900 in data/validation/dogs
+So that we have 1000 training examples for each class, and 400 validation examples for each class.
+In summary, this is our directory structure:
+```
+data/
+    train/
+        dogs/
+            dog001.jpg
+            dog002.jpg
+            ...
+        cats/
+            cat001.jpg
+            cat002.jpg
+            ...
+    validation/
+        dogs/
+            dog001.jpg
+            dog002.jpg
+            ...
+        cats/
+            cat001.jpg
+            cat002.jpg
+            ...
+```
+'''
+
+import argparse
+
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras import applications
+
+
+def main(params):
+    datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # build the VGG16 network
+    model = applications.VGG16(include_top=False, weights='imagenet')
+
+    generator = datagen.flow_from_directory(
+        params.train_data_dir,
+        target_size=(params.img_height, params.img_width),
+        batch_size=params.batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_train = model.predict_generator(
+        generator, params.nb_train_samples // params.batch_size)
+    np.save(open('bottleneck_features_train.npy', 'wb'),
+            bottleneck_features_train)
+
+    generator = datagen.flow_from_directory(
+        params.val_data_dir,
+        target_size=(params.img_height, params.img_width),
+        batch_size=params.batch_size,
+        class_mode=None,
+        shuffle=False)
+    bottleneck_features_validation = model.predict_generator(
+        generator, params.nb_val_samples // params.batch_size)
+    np.save(open('bottleneck_features_validation.npy', 'wb'),
+            bottleneck_features_validation)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--train_data_dir', type=str, default='data/train',
+                        help="Path to directory with training data.")
+    parser.add_argument('--nb_train_samples', type=int, default=5216,
+                        help="Number of training samples.")
+    parser.add_argument('--val_data_dir', type=str, default='data/test',
+                        help="Path to directory with test data.")
+    parser.add_argument('--nb_val_samples', type=int, default=624,
+                        help="Number of test samples.")
+    parser.add_argument('--img_height', type=int, default=150,
+                        help="Images will be resized to this height.")
+    parser.add_argument('--img_width', type=int, default=150,
+                        help="Images will be resized to this width.")
+    parser.add_argument('--batch_size', type=int, default=16,
+                        help="Batch size.")
+    params = parser.parse_args()
+
+    main(params)
